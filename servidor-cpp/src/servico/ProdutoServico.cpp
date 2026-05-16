780f570133a0d34315c0858adadc4c0023ee8415
ArianeSilva10 <arianeeng2023@gmail.com>
2026-05-16 19:09:28 -0300

    adaptando serialização para json

--- servidor-cpp/src/servico/ProdutoServico.cpp
+++ servidor-cpp/src/servico/ProdutoServico.cpp
@@ -48,16 +48,14 @@ vector<VacinaPerecivel*> ProdutoServico::listarVacinasPereciveis() const {
     return vacinas;
 }
 
-string ProdutoServico::serializar(Produto* p) const {
-    string result = "";
-
-    result += "id=" + to_string(p->getId()) + ";";
-    result += "nome=" + p->getNome() + ";";
-    result += "preco=" + to_string(p->getPreco()) + ";";
-    result += "fabricante=" + p->getFabricante() + ";";
-
-    result += "tipo=" + string(typeid(*p).name()) + ";";
-    return result;
+json ProdutoServico::serializar(Produto* p) const {
+    json j;
+    j["id"] = p->getId();
+    j["nome"] = p->getNome();
+    j["preco"] = p->getPreco();
+    j["fabricante"] = p->getFabricante();
+    j["tipo"] = string(typeid(*p).name());
+    return j;
 }
 
 int ProdutoServico::getTotalProdutos() const {
